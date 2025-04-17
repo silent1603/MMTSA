94ebc312521d66de423648cfb55dfcd1857deef6
trung.daoquoc <trung.daoquoc@gameloft.com>
2025-04-17 18:29:47 +0700

    * add run bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @echo off
 
 python train.py dataEgo  RGB Sensor --arch=BNInception --train_list train_dataego_file --val_list test_dataego_file --visual_path data\DataEgo\images --save_stats
+python train.py MMAct  RGB AccPhone AccWatch Gyro Orie --arch=BNInception --train_list train_mmact_file --val_list test_mmact_file --visual_path data\MMAct\Image_subject --save_stats
